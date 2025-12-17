adb5da91d86b85e0d63c9b69088737b01b129d55
jakobneukirchner <118596991+jakobneukirchner@users.noreply.github.com>
2025-12-17 13:50:13 +0100

    ViewModel factory for dependency injection

---
+++ app/src/main/java/com/jakobneukirchner/perdis/viewmodel/PerdisViewModelFactory.kt
@@ -0,0 +1,32 @@
+package com.jakobneukirchner.perdis.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.jakobneukirchner.perdis.data.DienstplanRepository
+import com.jakobneukirchner.perdis.data.LoginRepository
+import com.jakobneukirchner.perdis.data.PDFRepository
+import com.jakobneukirchner.perdis.data.util.CredentialsManager
+import com.jakobneukirchner.perdis.data.util.WebViewScraperUtil
+
+class PerdisViewModelFactory(
+    private val context: Context
+) : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val credentialsManager = CredentialsManager(context)
+        val webViewScraperUtil = WebViewScraperUtil(context)
+        val loginRepository = LoginRepository(credentialsManager, webViewScraperUtil)
+        val dienstplanRepository = DienstplanRepository(webViewScraperUtil)
+        val pdfRepository = PDFRepository(context, webViewScraperUtil)
+
+        return when (modelClass) {
+            LoginViewModel::class.java ->
+                LoginViewModel(loginRepository) as T
+            DienstplanViewModel::class.java ->
+                DienstplanViewModel(dienstplanRepository) as T
+            else -> throw IllegalArgumentException("Unknown ViewModel: $modelClass")
+        }
+    }
+}
