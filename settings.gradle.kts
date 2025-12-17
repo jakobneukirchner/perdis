6f02d8fc0312863f1650af6c4afab84ead5cf501
jakobneukirchner <118596991+jakobneukirchner@users.noreply.github.com>
2025-12-17 15:04:03 +0100

    Add plugin versions to settings.gradle.kts pluginManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,11 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.6.0"
+        id("org.jetbrains.kotlin.android") version "2.0.0"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    }
 }
 
 dependencyResolutionManagement {
